9e6eb908ec4b32facaf87991b6b5eb1ae3746206
Vinícius do Carmo <vinicius.docarmo@wetransfer.com>
2024-10-01 10:30:50 +0200

    Use KV for checking booking status and improve auth

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,31 @@ import puppeteer, { BrowserWorker } from '@cloudflare/puppeteer';
 
 interface Env {
 	MYBROWSER: BrowserWorker;
+	WOD: KVNamespace;
 	EMAIL: string;
 	PASS: string;
+	TOKEN: string;
 }
 
 export default {
 	async fetch(request, env): Promise<Response> {
 		const auth = request.headers.get('Authorization')?.split('Bearer ')[1];
-		if (auth !== "vinimdocarmo@gmail.com") {
+		if (auth !== env.TOKEN) {
 			return new Response('Unauthorized', { status: 401 });
+		}
+
+		// get year, month and day from today's date
+		const today = new Date();
+		const year = today.getFullYear();
+		const month = today.getMonth() + 1;
+		const day = today.getDate();
+		const time = "1800-1900";
+
+		const bookedKey = `booked:${year}-${month}-${day}-${time}`;
+
+		// check if today's WOD is already booked
+		if (await env.WOD.get(bookedKey) === "1") {
+			return Response.json({ alreadyBooked: true });
 		}
 
 		const browser = await puppeteer.launch(env.MYBROWSER, { keep_alive: 10000 });
@@ -58,7 +74,6 @@ export default {
 		const alertSuccess = await page.$('.alert.success');
 
 		if (alertSuccess) {
-			console.log('class already booked!');
 			await browser.close();
 			return Response.json({ ok: 'class already booked!' });
 		}
@@ -78,6 +93,10 @@ export default {
 			await signUpSpan.click();
 			await page.waitForSelector('.alert.success');
 			await browser.close();
+
+			// mark today's WOD as booked
+			await env.WOD.put(bookedKey, "1");
+
 			return Response.json({ ok: 'class booked!' });
 		} else {
 			await browser.close();
