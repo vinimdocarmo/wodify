70eb138edb8ce8c835da34a17fb1637d48808323
Vinícius do Carmo <vinicius.docarmo@wetransfer.com>
2024-10-10 14:32:56 +0200

    Add prettier config file

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+// create a basic prettier configuration file
+module.exports = {
+    singleQuote: false,
+    trailingComma: 'all',
+    arrowParens: 'always',
+    printWidth: 120,
+    tabWidth: 2,
+};
